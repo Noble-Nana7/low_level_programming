133edd26f6c4498c910f7d1e2f321f0a2c8df801
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-10-07 02:23:41 -0700

    i did this!

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This code will print the lowercase alphabet
+ * backwards.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int start = 122;
+	int end = 97;
+
+	int i;
+
+	for (i = start; i >= end; --i)
+	{
+		putchar(i);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
