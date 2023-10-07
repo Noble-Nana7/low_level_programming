f9f572fc5f588f99e8cf30aff0dbe7e1de199d06
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-10-06 18:20:06 -0700

    this is new!

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* This code will print out random
+ * pseudo-numbers at every instance of 
+ * n and obey each condition
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else 
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
