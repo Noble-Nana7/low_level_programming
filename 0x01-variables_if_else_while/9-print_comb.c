8b4cdea1cb653cb660c2f166e02b107f09e47100
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-10-07 03:16:47 -0700

    i like!

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This code printsall possible combinations of 
+ * single-digit numbers. each spe by a space and a comma.
+ * prints to stdout
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int start = 0;
+	int end = 9;
+
+	int i;
+
+	for (i = start; i <= end; ++i)
+	{
+		putchar(i + '0');
+		
+		if (i < end)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
