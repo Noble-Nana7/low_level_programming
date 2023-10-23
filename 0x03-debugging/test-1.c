194bcbd58155d98c21bc47fb42484288f1c5c0cb
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-10-22 17:35:22 -0700

    jl

---
+++ 0x03-debugging/test-1.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d is a negative number\n", i);
+	}
+	else if (i > 0)
+	{
+		printf("%d is a positive number\n", i);
+	}
+	else (i = 0); 
+	{
+		printf("%d is zero, moron\n", i);
+	}
+}
