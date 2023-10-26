1e5fd10b2a8bba1d43994aee062c025f80461854
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-10-26 11:05:35 -0700

    commit 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - A fucntion that caalculates and prins the
+ * length of a string.
+ * @s: The poiter to the string that will be calculated.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	return 1 + _strlen_recursion(s + 1);
+}
