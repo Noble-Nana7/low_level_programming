672440142ba88d16c007f787a04ae992131d5341
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-10-26 03:34:51 -0700

    -message

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function to recursively print a string 
+ * followed by a new line
+ * @s: a char that points to a string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0') 
+	{
+		/*The base case - to check for the null terminator*/
+		
+		_putchar('\n');
+		return; /*this return holds no value because the function has a void rt*/
+	}
+
+	/*now i print the current char from the recursion*/
+	_putchar(*s);
+
+	/*caalling the func again to ptc the rest of the string*/
+	_puts_recursion(s +1);
+}
+
+/**
+ * main - the entry point of this function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "Nnaemeka will grow past this level";
+
+	_puts_recursion(str);
+
+	return 0;
+}
