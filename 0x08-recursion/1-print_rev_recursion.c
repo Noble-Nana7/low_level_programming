c66b2fd196c7ac9b8bbb5d38b098ba960ead8ab2
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-10-26 10:33:38 -0700

    commit 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - This function prints a string in
+ * reverse.
+ * @s: the pointer to the string to be printed.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
