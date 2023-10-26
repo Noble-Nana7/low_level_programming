939f0f51a54bab93ea7d6f9493da859afc738b8a
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-10-26 04:04:56 -0700

    commit 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * _puts_recursion - a function to recursively print a string 
+ * _puts_recursion - a function to recursively print a string
  * followed by a new line
  * @s: a char that points to a string
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') 
+	if (*s == '\0')
 	{
 		/*The base case - to check for the null terminator*/
-		
 		_putchar('\n');
 		return; /*this return holds no value because the function has a void rt*/
 	}
@@ -20,5 +19,5 @@ void _puts_recursion(char *s)
 	_putchar(*s);
 
 	/*caalling the func again to ptc the rest of the string*/
-	_puts_recursion(s +1);
+	_puts_recursion(s + 1);
 }
