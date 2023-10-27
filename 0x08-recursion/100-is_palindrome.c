dfaf42b4fa3a7abd3d8929aab1ac50820f2f5cdd
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-10-27 12:24:40 -0700

    commit 100!

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,19 +8,16 @@
  *
  * Return: 1 if the substring is a palindrome, 0 otherwise.
  */
-int is_palindrome_helper(char *start, char *end) {
+int is_palindrome_helper(char *start, char *end)
+{
 	/* Base case: If the substring has 0 or 1 character, it's a palindrome */
-	if (start >= end) {
-		return 1;
-	}
-
+	if (start >= end)
+		return (1);
 	/* Check if the first and last characters are equal */
-	if (*start != *end) {
-		return 0; /* Not a palindrome */
-	}
-
+	if (*start != *end)
+		return (0); /* Not a palindrome */
 	/* Recursively check the substring between the first and last characters */
-	return is_palindrome_helper(start + 1, end - 1);
+	return (is_palindrome_helper(start + 1, end - 1));
 }
 
 /**
@@ -29,7 +26,8 @@ int is_palindrome_helper(char *start, char *end) {
  *
  * Return: 1 if the string is a palindrome, 0 otherwise.
  */
-int is_palindrome(char *s) {
+int is_palindrome(char *s)
+{
 	/* Call the helper function with appropriate pointers */
-	return is_palindrome_helper(s, s + strlen(s) - 1);
+	return (is_palindrome_helper(s, s + strlen(s) - 1));
 }
