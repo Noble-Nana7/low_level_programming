a31d1b65deaf20e4f19eb36d8128da698e84f47a
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-11-04 06:42:28 -0700

    commmit 5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point of the program
+* @argc: The number of command line arguments
+* @argv: An array containing the command line arguments
+* Return: 0 on success, 1 on error
+*/
+int main(int argc, char *argv[])
+{
+int sum = 0;
+int i, j;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
+}
