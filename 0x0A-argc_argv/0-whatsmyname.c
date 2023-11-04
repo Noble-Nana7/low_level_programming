894037a17a2e4cca9eee32793fe3640ceb02f6fc
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-11-04 06:32:30 -0700

    commit 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - prints the name of the program
+* @argc: argument count
+* @argv: argument vector
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+(void)argc; /* Suppress unused parameter warning */
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is for argc and argv
