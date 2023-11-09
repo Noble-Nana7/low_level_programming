cb84575ef2088ed5a9fdb35e8d0e32a555220f3b
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-11-09 07:50:50 -0800

    commit 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: always 0 (Success).
+ */
+
+int main()
+{
+	printf("This curent file is named: %s\n", __FILE__);
+	return (0);
+}
