e891d7e17fc7626e6f932a32413e2bf8c3fd5074
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-11-22 13:06:30 -0800

    hello1

--- 0x02-functions_nested_loops/README.md
+++
@@ -1,1 +0,0 @@
-this is a dir for the part 1 of functions and nested loops!
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,25 @@
-#ifndef MAIN_H	
+#ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+/**
+ * File: main.h
+ *
+ * Description: Header file containing description for all functions
+ */
+
+int _putchar(char a);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
 
 #endif
