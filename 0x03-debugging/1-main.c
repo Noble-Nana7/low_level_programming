0e5af7c5b32f1a12857d3669771a1bd37080643c
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-11-22 13:33:51 -0800

    commit 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
+ * main - causes sn infinite loop
  * Return: 0
  */
 
@@ -12,13 +12,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	/*while (i < 10)*/
-	{
-		putchar(i);
-	}
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
