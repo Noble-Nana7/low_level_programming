8e76e579d223edb28f19c9b46d5607225a120417
Noble-Nana7 <nnaemekanoble7@gmail.com>
2023-11-22 13:32:04 -0800

    commit 1

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first number
- * @b: second number
- * @c: third number
+ * largest_number - returns the largest of three numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest number
  */
 
@@ -25,5 +25,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-	return largest;
+	return (largest);
 }
